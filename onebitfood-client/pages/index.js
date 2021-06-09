fd736de7858c8090829233474c7851d786253d3a
JuniimSI <jrfernandesti@gmail.com>
2021-06-09 15:57:39 -0300

    Improve styles initials

--- onebitfood-client/pages/index.js
+++ onebitfood-client/pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Container from 'react-bootstrap/Container'
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <Container>
+      Home Page :)
+    </Container>
   )
 }
