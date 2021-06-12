c935a5a0c9abdc8680d1aa66ebb1b09b5d0163a3
JuniimSI <jrfernandesti@gmail.com>
2021-06-12 12:15:29 -0300

    Receive data from api

--- onebitfood-client/components/ListRestaurants/index.js
+++ onebitfood-client/components/ListRestaurants/index.js
@@ -1,42 +1,12 @@
 import {Row, Col} from 'react-bootstrap';
 import Restaurant from './Restaurant';
+import getRestaurants from '../../services/getRestaurants';
 
 export default function ListRestaurants() {
+  const {restaurants, isLoading, isError } = getRestaurants();  
 
-  const restaurants = [
-      {
-         'id': 1,
-         'name': 'example 1',
-         'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-         'delivery_tax': '5',
-         'image_url': '/restaurant.jpeg',
-         'category_title': 'Cozinha japonesa'
-      },
-      {
-         'id': 2,
-         'name': 'example 2',
-         'delivery_tax': '10',
-         'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-         'image_url': '/restaurant.jpeg',
-         'category_title': 'Cozinha mineira'
-      },
-      {
-         'id': 3,
-         'name': 'example 3',
-         'delivery_tax': '15',
-         'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-         'image_url': '/restaurant.jpeg',
-         'category_title': 'Cozinha vegana'
-      },
-      {
-         'id': 4,
-         'name': 'example 4',
-         'delivery_tax': '10',
-         'description': 'Javascript Ipsum, Javascript Ipsum e Javascript Ipsum Javascript Ipsum, Javascript Ipsum e Javascript Ipsum',
-         'image_url': '/restaurant.jpeg',
-         'category_title': 'Cozinha vegana'
-      }
-    ]
+  if(!restaurants)
+    return null;
 
   return(
     <div className="mt-5">
