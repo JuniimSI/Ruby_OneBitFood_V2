0206056219d6169253d2dd59ec44499087c44c15
JuniimSI <jrfernandesti@gmail.com>
2021-06-12 16:59:46 -0300

    Add searchbox

--- onebitfood-client/components/Header/index.js
+++ onebitfood-client/components/Header/index.js
@@ -1,10 +1,11 @@
 import {Navbar} from 'react-bootstrap';
 import Image from 'next/image';
 import Link from 'next/link';
+import SearchBox from '../SearchBox';
 
 export default function Header() {
     return(
-        <Navbar bg="white" expand="lg" className="border-bottom border-custom-gray">
+        <Navbar bg="white" expand="lg" className="border-bottom border-custom-gray ">
             <Navbar.Brand className="mx-4 mt-1 clickable-effect">
             <Link href="/restaurants">
             <a>
@@ -17,6 +18,10 @@ export default function Header() {
                 </a>
             </Link>
             </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-navz"/>
+             <Navbar.Collapse id="responsive-navbar-navz" className="justify-content-end">
+               <SearchBox />
+             </Navbar.Collapse>
         </Navbar>
     )
 }
