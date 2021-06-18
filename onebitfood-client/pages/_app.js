6ecbc52a7053ccf1688298be5d1b86fcfcd02b12
JuniimSI <jrfernandesti@gmail.com>
2021-06-18 09:39:15 -0300

    implemented details restaurants

--- onebitfood-client/pages/_app.js
+++ onebitfood-client/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss'
 import Head from 'next/head'
 import Header from '../components/Header';
 import Container from 'react-bootstrap/Container'
+import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,7 +15,9 @@ function MyApp({ Component, pageProps }) {
       <main>
         <Header />
         <Container className="mt-5">
-          <Component {...pageProps} />
+          <RecoilRoot>
+            <Component {...pageProps} />
+          </RecoilRoot>
         </Container>
       </main>
     </>
