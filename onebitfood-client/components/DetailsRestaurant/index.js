7e37a6c68728a24b5b8d44c6cb9bdc6c76103dc8
JuniimSI <jrfernandesti@gmail.com>
2021-06-26 15:02:56 -0300

    Added final changes and add SSG

--- onebitfood-client/components/DetailsRestaurant/index.js
+++ onebitfood-client/components/DetailsRestaurant/index.js
@@ -1,29 +1,18 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Details from './Details';
 import CategoryProducts from './CategoryProducts';
-import getRestaurant from '../../services/getRestaurant';
-import { useRouter } from 'next/router';
-import { Spinner, Alert } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
 
 export default function DetailsRestaurant(props) {
 
-    const router = useRouter();
-    const { id } = router.query;
+ if(props.isError)
+   return <Alert variant='custom-red'>Erro ao carregar</Alert>
 
-    const { restaurant, isLoading, isError } = getRestaurant(id);
-
-    if(isError)
-        return <Alert variant='custom-red'>Erro ao carregar</Alert>
-    else if(isLoading)
-        return <Spinner animation='border'/>
-
-    return(
-            <>
-                <Details {...restaurant} />
-                {restaurant.product_categories.map((product_category, i) =>
-                    <CategoryProducts restaurant={restaurant} {...product_category} key={i} />
-                )}
-            </>
-        )
+ return(
+   <>
+     <Details {...props.restaurant} />
+    {props.restaurant.product_categories.map((product_category, i) =>
+       <CategoryProducts restaurant={props.restaurant} {...product_category} key={i} />
+    )}
+   </>
+  )
 }
